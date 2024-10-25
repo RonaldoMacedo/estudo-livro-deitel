a279f29241768385f22b0812637ebb8a082fbad7
RonaldoMacedo <ronni29_100@hotmail.com>
2024-10-24 22:53:02 -0300

    2.6: múltiplas linhas com printf

--- src/capitulo2/Welcome1.java
+++ src/capitulo2/Welcome1.java
@@ -4,7 +4,7 @@ public class Welcome1 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Welcome\nto\nJava\nProgramming!");
+		System.out.printf("%s%n%s%n", "Welcome to", "Java Programming!");
 
 	}
 
