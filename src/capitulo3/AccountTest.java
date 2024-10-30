c953eb6d87654dca854c80dbafab9c4577d6f106
RonaldoMacedo <ronni29_100@hotmail.com>
2024-10-29 23:00:53 -0300

    3.9: entrada e saída de números de ponto flutuante com objetos Account

--- src/capitulo3/AccountTest.java
+++ src/capitulo3/AccountTest.java
@@ -1,14 +1,37 @@
 package capitulo3;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class AccountTest {
 
 	public static void main(String[] args) {
 		
-		Account account1 = new Account("Jane Green");
-		Account account2 = new Account("John Blue");
+		Locale.setDefault(Locale.US);
 		
-		System.out.printf("account1 name is %s%n", account1.getName());
-		System.out.printf("account2 name is %s%n", account2.getName());
+		Account account1 = new Account("Jane Green", 50.00);
+		Account account2 = new Account("John Blue", -7.53);
+		
+		System.out.printf("%s balance: $%.2f %n", account1.getName(), account1.getBalance());
+		System.out.printf("%s balance: $%.2f %n%n", account2.getName(), account2.getBalance());
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter deposit amount for account1: ");
+		double depositAmount = input.nextDouble();
+		System.out.printf("%nadding %.2f to account1 balance%n%n", depositAmount);
+		account1.deposit(depositAmount);
+		
+		System.out.printf("%s balance: $%.2f %n", account1.getName(), account1.getBalance());
+		System.out.printf("%s balance: $%.2f %n%n", account2.getName(), account2.getBalance());
+		
+		System.out.print("Enter deposit amount for account2: ");
+		depositAmount = input.nextDouble();
+		System.out.printf("%nadding %.2f to account2 balance%n%n", depositAmount);
+		account2.deposit(depositAmount);
+		
+		System.out.printf("%s balance: $%.2f %n", account1.getName(), account1.getBalance());
+		System.out.printf("%s balance: $%.2f %n%n", account2.getName(), account2.getBalance());
 
 	}
 
