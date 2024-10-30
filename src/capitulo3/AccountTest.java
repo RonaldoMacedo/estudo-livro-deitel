7b45a325e039aec9610ec445e35cb9d85402ea6e
RonaldoMacedo <ronni29_100@hotmail.com>
2024-10-29 22:44:28 -0300

    3.6: usando o construtor de Account para inicializar a variável de instância name no momento em que cada objeto Account é criado

--- src/capitulo3/AccountTest.java
+++ src/capitulo3/AccountTest.java
@@ -1,23 +1,14 @@
 package capitulo3;
 
-import java.util.Scanner;
-
 public class AccountTest {
 
 	public static void main(String[] args) {
 		
-		Scanner input = new Scanner(System.in);
+		Account account1 = new Account("Jane Green");
+		Account account2 = new Account("John Blue");
 		
-		Account myAccount = new Account();
-		
-		System.out.printf("Initial name is: %s%n%n", myAccount.getName());
-		
-		System.out.println("Please enter the name:");
-		String theName = input.nextLine();
-		myAccount.setName(theName);
-		System.out.println();
-		
-		System.out.printf("Name in object myAccount is:%n%s%n", myAccount.getName());
+		System.out.printf("account1 name is %s%n", account1.getName());
+		System.out.printf("account2 name is %s%n", account2.getName());
 
 	}
 
