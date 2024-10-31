4b4b3c08914b115cd01794dd22771ade498352d3
RonaldoMacedo <ronni29_100@hotmail.com>
2024-10-31 15:35:13 -0300

    4.10: resolvendo o problema da média da classe usando a repetição controlada por sentinela

--- src/capitulo4/ClassAverage.java
+++ src/capitulo4/ClassAverage.java
@@ -1,28 +1,36 @@
 package capitulo4;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class ClassAverage {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
 		Scanner input = new Scanner(System.in);
 		
 		int total = 0;
-		int gradeCounter = 1;
+		int gradeCounter = 0;
 		
-		while(gradeCounter <= 10) {
-			System.out.print("Enter grade: ");
-			int grade = input.nextInt();
+		System.out.print("Enter grade or -1 to quit: ");
+		int grade = input.nextInt();
+		
+		while(grade != -1) {
 			total = total + grade;
 			gradeCounter = gradeCounter + 1;
+			
+			System.out.print("Enter grade or -1 to quit: ");
+			grade = input.nextInt();	
 		}
 		
-		int average = total / 10;
-		
-		System.out.printf("%nTotal of all 10 grades is %d%n", total);
-		System.out.printf("Class average is %d%n", average);
-
+		if(gradeCounter != 0) {
+			double average = (double)total / gradeCounter;
+			System.out.printf("%nTotal of the %d grades entered is %d%n", gradeCounter, total);
+			System.out.printf("Class average is %.2f%n", average);
+		}else {
+			System.out.println("No grades were entered");
+		}
 	}
 
 }
