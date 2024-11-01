e5d06318defa8884c0c41a1cdeb4b7c0ca38ae92
RonaldoMacedo <ronni29_100@hotmail.com>
2024-10-31 21:04:10 -0300

    5.6: cálculos de juros compostos com for

---
+++ src/capitulo5/Interest.java
@@ -0,0 +1,25 @@
+package capitulo5;
+
+import java.util.Locale;
+
+public class Interest {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		
+		double amount;
+		double principal = 1000.0;
+		double rate = 0.05;
+		
+		System.out.printf("%s%20s %n", "Year", "Amount on deposit");
+		
+		for(int year = 1; year <= 10; ++year) {
+			amount = principal * Math.pow(1.0 + rate, year);
+			
+			System.out.printf("%4d%,20.2f%n", year, amount);
+		}
+
+	}
+
+}
