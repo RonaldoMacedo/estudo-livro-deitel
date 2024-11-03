673db86cfb52d3ad076c4bd21068ca17dcf01f6e
RonaldoMacedo <ronni29_100@hotmail.com>
2024-11-03 16:07:57 -0300

    6.12: aplicativo de teste que exibe um rosto sorridente + acertos na classe DrawSmiley

--- src/capitulo6/DrawSmiley.java
+++ src/capitulo6/DrawSmiley.java
@@ -12,8 +12,11 @@ public class DrawSmiley extends JPanel{
 		
 		g.setColor(Color.YELLOW);
 		g.fillOval(10, 10, 200, 200);
-		g.fillOval(135, 65, 30, 30);
 		
+		g.setColor(Color.BLACK);
+		g.fillOval(55, 65, 30, 30);
+		g.fillOval(135, 65, 30, 30);
+
 		g.fillOval(50, 110, 120, 60);
 		
 		g.setColor(Color.YELLOW);
