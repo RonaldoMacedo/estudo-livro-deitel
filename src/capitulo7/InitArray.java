685159afcf72e7c74becb5eeeedce979220f4c08
RonaldoMacedo <ronni29_100@hotmail.com>
2024-11-03 19:30:13 -0300

    7.3: inicializando os elementos de um array com um inicializador de array

--- src/capitulo7/InitArray.java
+++ src/capitulo7/InitArray.java
@@ -4,7 +4,7 @@ public class InitArray {
 
 	public static void main(String[] args) {
 		
-		int[] array = new int[10];
+		int[] array = {32, 27, 64, 18, 95, 14, 90, 70, 60, 37};
 		
 		System.out.printf("%s%8s%n", "Index", "Value");
 		
