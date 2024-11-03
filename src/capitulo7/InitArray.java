fb1f86b2d52dcb880165c0ed4f05bfe5e941b5ce
RonaldoMacedo <ronni29_100@hotmail.com>
2024-11-03 19:34:35 -0300

    7.4: calculando os valores a serem colocados nos elementos de um array

--- src/capitulo7/InitArray.java
+++ src/capitulo7/InitArray.java
@@ -4,7 +4,12 @@ public class InitArray {
 
 	public static void main(String[] args) {
 		
-		int[] array = {32, 27, 64, 18, 95, 14, 90, 70, 60, 37};
+		final int ARRAY_LENGTH = 10;
+		int[] array = new int[ARRAY_LENGTH];
+		
+		for(int counter = 0; counter < array.length; counter++) {
+			array[counter] = 2 + 2 * counter;
+		}
 		
 		System.out.printf("%s%8s%n", "Index", "Value");
 		
