9cc4810970247e9c6d7ceeed4dd93e3b5e3eb2c5
RonaldoMacedo <ronni29_100@hotmail.com>
2024-11-10 22:46:04 -0300

    7.17: inicializando arrays bidimensionais

--- src/capitulo7/InitArray.java
+++ src/capitulo7/InitArray.java
@@ -4,19 +4,22 @@ public class InitArray {
 
 	public static void main(String[] args) {
 		
-		final int ARRAY_LENGTH = 10;
-		int[] array = new int[ARRAY_LENGTH];
+		int[][] array1 = {{1, 2, 3}, {4, 5, 6}};
+		int[][] array2 = {{1, 2}, {3}, {4, 5, 6}};
 		
-		for(int counter = 0; counter < array.length; counter++) {
-			array[counter] = 2 + 2 * counter;
-		}
+		System.out.println("Values in array1 by row are");
+		outputArray(array1);
 		
-		System.out.printf("%s%8s%n", "Index", "Value");
-		
-		for(int counter = 0; counter < array.length; counter++) {
-			System.out.printf("%5d%8d%n", counter, array[counter]);
-		}
-
+		System.out.printf("%nValues in array2 by row are%n");
+		outputArray(array2);
 	}
-
-}
+		
+		public static void outputArray(int[][] array) {
+		
+			for(int row = 0; row < array.length; row++) {
+				for(int column = 0; column < array[row].length; column++)
+					System.out.printf("%d  ", array[row][column]);
+				System.out.println();
+			}
+		}
+	}
