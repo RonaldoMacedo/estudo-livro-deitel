ba116b24c6273ac615021568a1315593f16fe3ba
RonaldoMacedo <ronni29_100@hotmail.com>
2024-11-10 23:11:43 -0300

    7.19: GradeBookTest cria o objeto GradeBook utilizando um array bidimensional de notas e invoca o método processGrades para analisá-las

--- src/capitulo7/GradeBookTest.java
+++ src/capitulo7/GradeBookTest.java
@@ -8,7 +8,11 @@ public class GradeBookTest {
 		
 		Locale.setDefault(Locale.US);
 		
-		int[] gradesArray = { 87, 68, 94, 100, 83, 78, 85, 91, 76, 87 };
+		int[][] gradesArray = {{87, 96, 70}, {68, 87, 90},
+							   {94, 100, 90}, {100, 81, 82},
+							   {83, 65, 85},{78, 87, 65},
+							   {85, 75, 83}, {91, 94, 100},
+							   {76, 72, 84}, {87, 93, 73}};
 		
 		GradeBook myGradeBook = new GradeBook("CS101 Introduction to Java Programming", gradesArray);
 		
