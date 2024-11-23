9fae90d66c33831fafc7a80841f494e4efcfe1dc
RonaldoMacedo <ronni29_100@hotmail.com>
2024-11-23 17:08:48 -0300

    8.13: a demonstração do membro static

--- src/capitulo8/EmployeeTest.java
+++ src/capitulo8/EmployeeTest.java
@@ -4,12 +4,18 @@ public class EmployeeTest {
 
 	public static void main(String[] args) {
 		
-		Date birth = new Date(7, 24, 1949);
-		Date hire = new Date(3, 12, 1988);
+		System.out.printf("Employees before instantiation: %d%n", Employee.getCount());
 		
-		Employee employee = new Employee("Bob", "Blue", birth, hire);
+		Employee e1 = new Employee("Susan", "Baker");
+		Employee e2 = new Employee("Bob", "Blue");
 		
-		System.out.println(employee);
+		System.out.printf("%nEmployees after instantiation:%n");
+		System.out.printf("via e1.getCount(): %d%n", e1.getCount());
+		System.out.printf("via e2.getCount(): %d%n", e2.getCount());
+		System.out.printf("via Employee.getCount(): %d%n", Employee.getCount());
+		
+		System.out.printf("%nEmployee 1: %s %s%nEmployee 2: %s %s%n", e1.getFirstName(), e1.getLastName(),
+																	  e2.getFirstName(), e2.getLastName());
 
 	}
 
