eeaa8b9d51301ca7f60b063ae49cc0b81aa192b0
RonaldoMacedo <ronni29_100@hotmail.com>
2024-11-23 21:26:43 -0300

    8.15: os membros de acesso de pacote de uma classe permanecem acessíveis a outras classe do mesmo pacote; 8.16: cálculos de juros compostos com BigDecimal

--- src/capitulo5/Interest.java
+++ src/capitulo5/Interest.java
@@ -1,5 +1,7 @@
 package capitulo5;
 
+import java.math.BigDecimal;
+import java.text.NumberFormat;
 import java.util.Locale;
 
 public class Interest {
@@ -8,16 +10,15 @@ public class Interest {
 		
 		Locale.setDefault(Locale.US);
 		
-		double amount;
-		double principal = 1000.0;
-		double rate = 0.05;
+		BigDecimal principal = BigDecimal.valueOf(1000.0);
+		BigDecimal rate = BigDecimal.valueOf(0.05);
 		
-		System.out.printf("%s%20s %n", "Year", "Amount on deposit");
+		System.out.printf("%s%20s%n", "Year", "Amount on deposit");
 		
 		for(int year = 1; year <= 10; ++year) {
-			amount = principal * Math.pow(1.0 + rate, year);
+			BigDecimal amount = principal.multiply(rate.add(BigDecimal.ONE).pow(year));
 			
-			System.out.printf("%4d%,20.2f%n", year, amount);
+			System.out.printf("%4d%20s%n", year, NumberFormat.getCurrencyInstance().format(amount));
 		}
 
 	}
