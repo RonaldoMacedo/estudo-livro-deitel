ca729fe9a36ed0dba4fb5bcd44110e6977b12555
RonaldoMacedo <ronni29_100@hotmail.com>
2024-11-28 15:40:05 -0300

    10.6: HourlyEmployee estende a classe Employee

--- src/capitulo10/SalariedEmployee.java
+++ src/capitulo10/SalariedEmployee.java
@@ -1,5 +1,39 @@
 package capitulo10;
 
-public class SalariedEmployee {
+import capitulo8.Employee;
+
+public class SalariedEmployee extends Employee {
+	
+	private double weeklySalary;
+
+	public SalariedEmployee(String firstName, String lastName, String socialSecurityNumber, double weeklySalary) {
+		super(firstName, lastName, socialSecurityNumber);
+		
+		if(weeklySalary < 0.0)
+			throw new IllegalArgumentException("Weekly salary must be >= 0.0");
+		
+		this.weeklySalary = weeklySalary;
+	}
+	
+	public void setWeeklySalary(double weeklySalary) {
+		if(weeklySalary < 0.0)
+			throw new IllegalArgumentException("Weekly salary must be >= 0.0");
+		
+		this.weeklySalary = weeklySalary;
+	}
+	
+	public double getWeeklySalary() {
+		return weeklySalary;
+	}
+
+	@Override
+	public double earnings() {
+		return getWeeklySalary();
+	}
+	
+	public String toString() {
+		return String.format("Salaried employee: %s%n%s: $%,.2f", super.toString(),
+							 "weekly salary", getWeeklySalary());
+	}
 
 }
