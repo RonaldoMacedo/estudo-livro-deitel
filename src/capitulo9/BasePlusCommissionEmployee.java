f5d0a4ab161e9aa2c428b92ea2b8fdf4b1ba75c5
RonaldoMacedo <ronni29_100@hotmail.com>
2024-11-28 09:59:35 -0300

    9.8: os membros private da superclasse não podem ser acessados em uma subclasse

--- src/capitulo9/BasePlusCommissionEmployee.java
+++ src/capitulo9/BasePlusCommissionEmployee.java
@@ -1,68 +1,18 @@
 package capitulo9;
 
-public class BasePlusCommissionEmployee {
+public class BasePlusCommissionEmployee extends CommissionEmployee{
 	
-	private final String firstName;
-	private final String lastName;
-	private final String socialSecurityNumber;
-	private double grossSales;
-	private double commissionRate;
 	private double baseSalary;
 	
 	public BasePlusCommissionEmployee(String firstName, String lastName, String socialSecurityNumber, double grossSales,
 			double commissionRate, double baseSalary) {
 		
-		if(grossSales < 0.0)
-			throw new IllegalArgumentException("Gross sales must be >= 0.0");
-		
-		if(commissionRate <= 0.0 || commissionRate >= 1.0)
-			throw new IllegalArgumentException("Commission rate must be > 0.0 and < 1.0");
+		super(firstName, lastName, socialSecurityNumber, grossSales, commissionRate);
 		
 		if(baseSalary < 0.0)
 			throw new IllegalArgumentException("Base salary must be >= 0.0");
 
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.socialSecurityNumber = socialSecurityNumber;
-		this.grossSales = grossSales;
-		this.commissionRate = commissionRate;
 		this.baseSalary = baseSalary;
-	}
-	
-	public String getFirstName() {
-		return firstName;
-	}
-	
-	public String getLastName() {
-		return lastName;
-	}
-	
-	public String getSocialSecurityNumber() {
-		return socialSecurityNumber;
-	}
-	
-	public void setGrossSales(double grossSales) {
-		
-		if(grossSales < 0.0)
-			throw new IllegalArgumentException("Gross sales must be >= 0.0");
-		
-		this.grossSales = grossSales;
-	}
-
-	public double getGrossSales() {
-		return grossSales;
-	}
-
-	public double getCommissionRate() {
-		return commissionRate;
-	}
-
-	public void setCommissionRate(double commissionRate) {
-		
-		if(commissionRate <= 0.0 || commissionRate >= 1.0)
-			throw new IllegalArgumentException("Commission rate must be > 0.0 and 1.0");
-		
-		this.commissionRate = commissionRate;
 	}
 
 	public void setBaseSalary(double baseSalary) {
@@ -77,6 +27,7 @@ public class BasePlusCommissionEmployee {
 		return baseSalary;
 	}
 	
+	@Override
 	public double earnings() {
 		return baseSalary + (commissionRate * grossSales);
 	}
